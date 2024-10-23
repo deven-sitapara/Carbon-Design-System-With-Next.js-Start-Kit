98e1189ea1969a504fbad3293b39ef0011446d46
Deven Sitapara <shriramsoft@gmail.com>
2024-10-23 18:27:19 +0530

    config updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    sassOptions: {
+      implementation: "sass-embedded",
+    },
+  };
 
 module.exports = nextConfig;
