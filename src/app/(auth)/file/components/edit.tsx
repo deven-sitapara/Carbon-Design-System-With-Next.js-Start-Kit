64ea715778b43f8d44ccf08c942433c516c17249
Deven S <shriramsoft@gmail.com>
2024-11-20 20:12:49 +0530

    build fix

--- src/app/(auth)/file/components/edit.tsx
+++ src/app/(auth)/file/components/edit.tsx
@@ -53,56 +53,65 @@ const EditButton: React.FC<{ row: DataTableRow<any[]> }> = ({ row }) => {
         </IconButton>
       )}
     >
-      {({ open, setOpen }) => (
-        <ComposedModal size="sm" open={open} onClose={() => setOpen(false)}>
-          <ModalHeader label="File Module" title={`Edit File #${row.id}`} />
-          <ModalBody>
-            <p style={{ marginBottom: "1rem" }}>
-              Edit File Record #{row.id} with the following data; the Date field
-              will be readonly.
-            </p>
-            <TextInput
-              data-modal-primary-focus
-              id="title-input"
-              labelText="Title of File"
-              placeholder="Title of File"
-              value={row.title}
-              style={{ marginBottom: "1rem" }}
+      {({ open, setOpen }) =>
+        rowObj &&
+        rowObj.title && (
+          <ComposedModal size="sm" open={open} onClose={() => setOpen(false)}>
+            <ModalHeader
+              label="File Module"
+              title={`Edit File #${rowObj.id}`}
             />
-            <Select
-              id="branch-select"
-              labelText="Branch"
-              value={row.branch}
-              style={{ marginBottom: "1rem" }}
+            <ModalBody>
+              <p style={{ marginBottom: "1rem" }}>
+                Edit File Record #{rowObj.id} with the following data; the Date
+                field will be readonly.
+              </p>
+              <TextInput
+                data-modal-primary-focus
+                id="title-input"
+                labelText="Title of File"
+                placeholder="Title of File"
+                value={rowObj?.title}
+                style={{ marginBottom: "1rem" }}
+              />
+              <Select
+                id="branch-select"
+                labelText="Branch"
+                value={rowObj?.branch}
+                style={{ marginBottom: "1rem" }}
+              >
+                {["Rajkot", "Morbi", "Bhavnagar", "Junagadh"].map((branch) => (
+                  <SelectItem key={branch} value={branch} text={branch} />
+                ))}
+              </Select>
+              <Select
+                id="company-select"
+                labelText="Company"
+                value={rowObj?.company}
+                style={{ marginBottom: "1rem" }}
+              >
+                {["HOME FIRST", "ADANI` HOUSING FINANCE"].map((company) => (
+                  <SelectItem key={company} value={company} text={company} />
+                ))}
+              </Select>
+              <TextInput
+                readOnly
+                id="date-input"
+                labelText="Date"
+                placeholder="Date"
+                value={rowObj.date}
+                style={{ marginBottom: "1rem" }}
+              />
+            </ModalBody>
+            <ModalFooter
+              primaryButtonText="Update"
+              secondaryButtonText="Cancel"
             >
-              {["Rajkot", "Morbi", "Bhavnagar", "Junagadh"].map((branch) => (
-                <SelectItem key={branch} value={branch} text={branch} />
-              ))}
-            </Select>
-            <Select
-              id="company-select"
-              labelText="Company"
-              value={row.company}
-              style={{ marginBottom: "1rem" }}
-            >
-              {["HOME FIRST", "ADANI` HOUSING FINANCE"].map((company) => (
-                <SelectItem key={company} value={company} text={company} />
-              ))}
-            </Select>
-            <TextInput
-              readOnly
-              id="date-input"
-              labelText="Date"
-              placeholder="Date"
-              value={row.date}
-              style={{ marginBottom: "1rem" }}
-            />
-          </ModalBody>
-          <ModalFooter primaryButtonText="Update" secondaryButtonText="Cancel">
-            <div></div>
-          </ModalFooter>
-        </ComposedModal>
-      )}
+              <div></div>
+            </ModalFooter>
+          </ComposedModal>
+        )
+      }
     </ModalStateManager>
   );
 };
